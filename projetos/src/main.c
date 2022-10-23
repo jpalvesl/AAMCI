0f5ee983aed34e85eb45b7cb1c0bf59f1ad136c3
João Lima <jpalvesl101@gmail.com>
2022-10-22 23:32:52 -0300

    tentando implementar futura atividade

--- projetos/src/main.c
+++ projetos/src/main.c
@@ -1,118 +1,80 @@
 #include "stm32f4xx.h"
 #include "Utility.h"
 
-void envia_cmd(uint8_t);	//função para enviar um comando no barramento
-uint8_t recebe_cmd(void);	//função para receber um comando
-void buzzer(void);			//função de ativação do buzzer
+#define TOGGLE_LED1 1
+#define TOGGLE_LED2 2
+#define ACK 3
+
+int teveComunicacao = 1;
+
+void buzzer(void);
+
+// ao receber um dado uma interrupcao de fim de comunicacao eh lancada e
+void USART1_IRQHandler(void)
+{
+	uint8_t pacote_recebido = USART1->DR;
+
+	if(pacote_recebido == TOGGLE_LED1) {
+		GPIOA->ODR ^= 1 << 6;
+		USART1->DR = ACK;
+	}
+
+	if(pacote_recebido == TOGGLE_LED2) {
+		GPIOA->ODR ^= 1 << 7;
+		USART1->DR = ACK;
+	}
+
+	if (pacote_recebido == ACK) {
+		teveComunicacao = 1;
+	}
+}
 
 int main(void)
 {
-	Configure_Clock();			//configura o sistema de clock
-	Delay_Start();				//inicializa funções de Delay
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOEEN; // Habilita o clock no barramento da porta GPIOA
+    Configure_Clock();
+    Delay_Start();
+    USART1_Init();
 
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOEEN;	//habilita o clock do GPIOA e GPIOE
+    GPIOA->ODR |= (1 << 7) | (1 << 6);
+	GPIOA->MODER |= (0b01 << 14) | (0b01 << 12) | (0b01 << 2);
+//    GPIOE->MODER	// Configurar PE4 e PE3 como entrada
+	GPIOE->PUPDR |= (0b01 << 8) | (0b01 << 6);
 
-	GPIOA->ODR |= (1<<7) | (1<<6) | 1;		//inicia com leds e buzzer desligados e linha COM em idle
-	GPIOA->OTYPER |= 1;						//saída open-drain em PA0
-	GPIOA->PUPDR |= 0b01;					//habilita pull-up em PA0
-	GPIOA->MODER |= (0b01 << 14) | (0b01 << 12) | (0b01 << 2) | (0b01) ; 	//pinos PA0, PA1, PA6 e PA7 no modo saída
-	GPIOE->PUPDR |= (0b01 << 8) | (0b01 << 6);								//habilita pull-up em PE4 e PE3
+	GPIOA->ODR &= ~(1 << 1);
 
-	Delay_ms(100);	//aguarda sinais estabilizarem
-	while(1)
-	{
-		if(!(GPIOE->IDR & (1 << 4)))			//verifica se PE4 foi pressionado
-		{
-			envia_cmd(0b01);
-			Delay_us(25);
-			if((GPIOA->IDR & 1)) {
+	Delay_ms(100); // aguarda os sinais estabilizarem // precisa
+	while(1) {
+		if (!(GPIOE->IDR & (1 << 4))) { // Verifica se PE4 foi pressionado
+			USART1->DR = TOGGLE_LED1;
+			//definir uma flag e caso nao seja alterada em x ms
+			teveComunicacao = 0;
+			Delay_us(10);
+
+			if (!teveComunicacao) {
 				GPIOA->ODR ^= 1 << 6;
 			}
+			//definir uma flag e caso nao seja alterada em x ms
 
 			buzzer();							//sinaliza o fim do envio
-			Delay_ms(75);					//filtro de bouncing
 			while(!(GPIOE->IDR & (1 << 4)));	//aguarda o botão ser solto
 		}
 
-		if(!(GPIOE->IDR & (1 << 3)))			//verifica se PE3 foi pressionado
-		{
-			envia_cmd(0b10);
-			Delay_us(25);
-			if((GPIOA->IDR & 1)) {
+		if (!(GPIOE->IDR & (1 << 3))) { // Verifica se PE3 foi pressionado
+			USART1->DR = TOGGLE_LED2;
+			//definir uma flag e caso nao seja alterada em x ms
+			teveComunicacao = 0;
+			Delay_us(10);
+
+			if (!teveComunicacao) {
 				GPIOA->ODR ^= 1 << 7;
 			}
+
+
 			buzzer();							//sinaliza o fim do envio
-			Delay_ms(75);					//filtro de bouncing
 			while(!(GPIOE->IDR & (1 << 3)));	//aguarda o botão ser solto
 		}
-
-		if(!(GPIOA->IDR & 1))	//verifica se há comunicação
-		{
-			uint8_t recebido = recebe_cmd();	//recebe o comando
-			if(recebido == 0b01)
-			{
-				GPIOA->ODR ^= 1 << 6;			//alterna o estado do LED em PA6
-			}
-			if(recebido == 0b10)
-			{
-				GPIOA->ODR ^= 1 << 7;			//alterna o estado do LED em PA7
-			}
-		}
 	}
-}
-
-//Função para envio de um comando
-void envia_cmd(uint8_t dado)
-{
-	GPIOA->ODR &= ~1;	//start bit
-	Delay_us(50);		//aguarda tempo do start bit
-	for(uint8_t counter=0; counter<2; ++counter) //envia os bits do comando
-	{
-		if(dado & 1)			//envia o próximo bit
-			GPIOA->ODR |= 1;
-		else
-			GPIOA->ODR &= ~1;
-		dado >>= 1;				//desloca os bits para envio posterior
-		Delay_us(50);			//aguarda o tempo do bit
-	}
-	GPIOA->ODR |= 1;	//stop bit
-	Delay_us(50);		//tempo do stop bit
-}
-
-//Função para recebimento de um comando
-uint8_t recebe_cmd(void)
-{
-	uint8_t dado_recebido = 0;
-	uint8_t dado_retornado = 0;
-	Delay_us(25);			//aguarda metade do start bit
-	if(!(GPIOA->IDR & 1))	//confirma que houve um start bit
-	{
-		for(uint8_t counter=0; counter<2; ++counter)	//leitura dos bits
-		{
-			Delay_us(50);				//aguarda o tempo do bit
-			if(GPIOA->IDR & 1)
-				dado_recebido |= 1;
-			else
-				dado_recebido &= ~1;
-			dado_recebido <<= 1;
-		}
-
-		Delay_us(50);			//aguarda para fazer leitura do stop bit
-		if((GPIOA->IDR & 1))	//confirma que houve um stop bit
-		{
-			dado_recebido >>= 1;
-			Delay_us(25);		//aguarda o fim do tempo do stop bit
-
-			GPIOA->ODR &= ~1;
-			Delay_us(50);
-			GPIOA->ODR |= 1;
-			return dado_retornado = ((dado_recebido & 0b10) >> 1) | ((dado_recebido & 0b01) << 1); //retorna o dado recebido
-		}
-		else
-			return 0;	//não houve recepção do stop bit, aborta recepção
-	}
-	else
-		return 0;		//não houve recepção do start bit, aborta recepção
 }
 
 //Função de sinalização de fim de envio de dado
